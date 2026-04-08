80eb035805b84c6066e4818fb3479e3c64ef12bc
JJ <DD>
2026-04-08 15:57:20 +0200

    Initial Commit

---
+++ PassMonitor/PassMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassMonitorApp.swift
+//  PassMonitor
+//
+//  Created by Juanjo Diaz de Argandoña on 08.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct PassMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
